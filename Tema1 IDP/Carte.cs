49246077f81c8b10a9f103cd6b17a3636a6fb1a6
Ionut <Ionut@DESKTOP-U341J5E>
2022-10-29 20:07:52 +0300

    Add project files.

---
+++ Tema1 IDP/Carte.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema1_IDP
+{
+     class Carte
+    {
+        public string NumeCarte;
+        public long ISBN;
+        public double pret;
+        public int Aparitie;
+        public string Editura;
+
+        public static Carte[] PreiaCarti()
+        {
+            Carte[] s_arr =
+            {
+                new Carte() {NumeCarte="Trei", Editura="Litera", Aparitie = 2022 , ISBN= 9786063395369 , pret = 69.90 },
+                new Carte() {NumeCarte="Arta de a citi oamenii", Editura="Litera", Aparitie = 2022  , ISBN= 9786063394911 , pret = 64.90 },
+                new Carte() {NumeCarte="Prima luna de razboi. Cronica evenimentelor", Editura="Humanitas", Aparitie = 2022  , ISBN= 9789735076542  , pret = 49.90 },
+                new Carte() {NumeCarte="Fizica povestita", Editura="Humanitas", Aparitie = 2022  , ISBN= 9789735076559 , pret = 119.00 },
+                new Carte() {NumeCarte="Armata nu se implica in conflict", Editura="Univers", Aparitie = 2020 , ISBN= 9789975319294 , pret = 25.00 },
+                new Carte() {NumeCarte="Evreii", Editura="Prut", Aparitie = 2020 , ISBN= 9789975545150 , pret = 40.00},
+                new Carte() {NumeCarte="Teoria haosului", Editura="Liberalis", Aparitie = 2020 , ISBN= 9786069488812 , pret = 25.00},
+                new Carte() {NumeCarte="Nascut in Zodia mortii", Editura="Andreas", Aparitie = 2017 , ISBN= 9786067650334 , pret = 28.00 },
+                new Carte() {NumeCarte="Actiunea interioara", Editura="Paralela 45", Aparitie = 1999 , ISBN= 9789739433945 , pret = 13.00},
+                new Carte() {NumeCarte="Retelele Infinitului", Editura="RAO", Aparitie = 1998 , ISBN= 9789739862660 , pret = 19.99},
+            };
+            return s_arr;
+        }
+        public static void AfisareCarti(List<Carte>carti)
+        {
+            foreach (Carte c in carti)
+                Console.WriteLine(c.NumeCarte + ", publicata de Editura " + c.Editura + " in anul " + c.Aparitie + ". ISBN:" + c.ISBN + ". Pret: " + c.pret + "RON");
+        }
+    }
+}
---
+++ Tema1 IDP/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema1_IDP
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Carte[] s_arr = Carte.PreiaCarti();
+            //Cerinta 1
+            
+            Console.WriteLine("Introduceti numele cartii pe care doriti sa o cautati: ");
+            string s1 = Console.ReadLine();
+            try
+            {
+                List<Carte> c1 = s_arr.Where(c => c.NumeCarte.Contains(s1)).ToList();
+                Carte.AfisareCarti(c1);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Cartea cautata nu se afla in lista.");
+            }
+
+            //Cerinta 2
+
+            Console.WriteLine("Introduceti anul de aparitie dorit: ");
+            int an = Convert.ToInt32(Console.ReadLine());
+           
+            List<Carte> c2 = s_arr.Where(c => c.Aparitie == an).ToList();
+            Console.WriteLine("Numarul de carti care au aparut in anul respectiv este: " + c2.Count);
+
+
+            //Cerinta 3
+
+            Console.WriteLine("Introduceti ISBN-ul dorit: ");
+            int isbn = Convert.ToInt32(Console.ReadLine());
+            List<Carte> c3 = new List<Carte>();
+            foreach (Carte c in s_arr)
+            {
+                string ex_isbn = c.ISBN.ToString();
+                string exx_isbn = isbn.ToString();
+                if(ex_isbn.StartsWith(exx_isbn))
+                {
+                    c3.Add(c);
+                }
+            }
+            Carte.AfisareCarti(c3);
+            
+
+            //Cerinta 4
+
+            try { 
+            Carte c4 = (Carte)s_arr.Where(c => c.Aparitie == 2020 && (c.Editura == "Humanitas"));
+                Console.WriteLine("\n Cartile care au aparut in anul 2020 si sunt publicate de editura Humanitas sunt: \n" + c4.NumeCarte);
+            }
+            catch
+            {
+                Console.WriteLine("\n Nu exista carti care au aparut in anul 2020 si sunt publicate de editura Humanitas");
+            }
+            
+            //Cerinta 5
+            Console.WriteLine("\n Cartile ordonate crescator dupa nume: \n");
+            List<Carte> c5 = s_arr.OrderBy(c => c.NumeCarte).ToList();
+            Carte.AfisareCarti(c5);
+
+            //Cerinta 6
+            Console.WriteLine("\n Cartile ordonate descrescator dupa anul aparitiei: \n");
+            List<Carte> c6 = s_arr.OrderByDescending(c => c.Aparitie).ToList();
+            Carte.AfisareCarti(c6);
+
+            //Cerinta 7
+            Console.WriteLine("\n Cartile care apartin editurilor Teora si Humanitas: \n");
+            List<Carte> c7 = s_arr.Where(c => c.Editura == "Teora" || c.Editura == "Humanitas").ToList();
+            Carte.AfisareCarti(c7);
+
+            //Cerinta 8
+            bool b1 = s_arr.All(c => c.Aparitie > 2020);
+            Console.WriteLine("\nToate cartile" + (b1? " au" : " nu au") + " aparut dupa anul 2020");
+
+            //Cerinta 9
+            Console.WriteLine("\nLista in care numele cartilor contin cuvantul copil: \n");
+            List<Carte> c9 = s_arr.Where(c => c.NumeCarte.Contains("copil")).ToList();
+            Carte.AfisareCarti(c9);
+        }
+    }
+}
